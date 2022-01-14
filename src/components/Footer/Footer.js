b351872946bf415a0174b4a35c8831d62ae9f7f5
zemira kehati <kehati10@gmail.com>
2022-01-14 07:34:22 +0200

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,18 @@
-function Footer({ link, text, onSignOut, loggedin, email }) {
+function Footer() {
   return (
     <footer className="footer">
       <div className="footer__copyrights">
         © 2021 Supersite, Powered by News API
       </div>
       <div className="footer__nav">
-        <div className="footer__nav_links">
+        <nav className="footer__nav_links">
           <a className="footer__nav_home" href="#home">
             Home
           </a>
           <a className="footer__nav_practicum" href="#home">
             Practicum by Yandex
           </a>
-        </div>
+        </nav>
         <div className="footer__nav_social">
           <a className="footer__nav_whatsapp" href="#home">
             {""}
